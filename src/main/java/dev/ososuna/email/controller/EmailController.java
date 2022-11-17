ae71455162b28b52bfc25c9afbd636845e6df6ce
oswi <oswaldo.adrian@live.com.mx>
2022-11-17 16:36:55 -0600

    cors config

--- src/main/java/dev/ososuna/email/controller/EmailController.java
+++ src/main/java/dev/ososuna/email/controller/EmailController.java
@@ -2,6 +2,7 @@ package dev.ososuna.email.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class EmailController {
   }
 
   @PostMapping
+  @CrossOrigin(origins = "*")
   public ResponseEntity<String> sendEmail(@RequestBody EmailDetails emailDetails) {
     return new ResponseEntity<>(emailService.sendSimpleMail(emailDetails), HttpStatus.OK);
   }
